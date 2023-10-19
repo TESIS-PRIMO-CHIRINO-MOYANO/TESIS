d0020d9bc5ce0f88d911b94b424f9a7d5b787f6f
tomassmoyano <tomasmoyanocba@gmail.com>
2023-10-19 15:53:00 -0300

    Solucion de estilos y errores inicio,nosotros,productos

--- FRONTEND/pryGestionDeAgua_Cliente/src/app/Components/layout/layout-routing.module.ts
+++ FRONTEND/pryGestionDeAgua_Cliente/src/app/Components/layout/layout-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
-import { InicioComponent } from './inicio/inicio.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
 import { ProductosComponent } from './Pages/productos/productos.component';
-
-import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { SobreNosotrosComponent } from './Pages/sobre-nosotros/sobre-nosotros.component';
 
 const routes: Routes = [
   {
