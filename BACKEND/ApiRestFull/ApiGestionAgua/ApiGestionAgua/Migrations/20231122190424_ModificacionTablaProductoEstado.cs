592c3983f4ceb3329697a6754a0f255aa8fa6a13
tomassmoyano <tomasmoyanocba@gmail.com>
2023-11-22 17:18:09 -0300

    creacion de interfaces y dto Producto

---
+++ BACKEND/ApiRestFull/ApiGestionAgua/ApiGestionAgua/Migrations/20231122190424_ModificacionTablaProductoEstado.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiGestionAgua.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModificacionTablaProductoEstado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Estado",
+                table: "Producto",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Producto");
+        }
+    }
+}
---
+++ BACKEND/ApiRestFull/ApiGestionAgua/ApiGestionAgua/Repositorio/IRepositorio/IProductoRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using ApiGestionAgua.Modelos;
+
+namespace ApiGestionAgua.Repositorio.IRepositorio
+{
+    public interface IProductoRepositorio
+    {
+
+        ICollection<Producto> GetProductos();
+
+        Producto GetProducto(int id);
+
+        bool ExisteProducto(string Nombre);
+
+        bool ExisteProducto(int IdProducto);
+        bool CrearProducto(Producto producto);
+
+        bool ActualizarProducto(Producto producto);
+
+        bool BorrarProducto(Producto producto);
+
+        bool Guardar();
+    }
+}
